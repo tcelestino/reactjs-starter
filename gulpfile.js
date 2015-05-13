e4f1c2d5befc1342ce2e13b900110d182b6923b5
Tiago Celestino <tcfarias@sp.r7.com>
2015-05-13 11:47:04 -0300

    create new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,44 @@
 var gulp      = require('gulp');
 var plugins   = require('gulp-load-plugins')();
-var react     = require('gulp-react');
 
 var SOURCE = {
   htmlPath: '*.html',
   vendorPath: 'src/js/vendor/**/*.js',
-  jsPath: 'src/js/**/*.js'
+  jsPath: 'src/js/**/*.js',
+  jsxPath: 'src/jsx/*'
 };
 
 var BUILD = {
   htmlPath: '.',
-  jsPath: 'dist/js/',
+  jsPath: 'dist/js',
 };
 
 /* DEV TASKS */
-
 gulp.task('js-dev', function () {
-  return gulp.src('src/js/jsx/App.jsx')
+  'use strict';
+  return gulp.src('src/jsx/*.jsx')
+    .pipe(plugins.plumber())
     .pipe(plugins.react())
-    //.pipe(jshint('.jshintrc'))
-    //.pipe(jshint.reporter('default'))
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.jshint.reporter('default'))
     .pipe(gulp.dest('src/js'));
 });
 
+gulp.task('dev', function () {
+  'use strict';
+  gulp.start('js-dev');
+});
+
+gulp.task('watch-dev', function () {
+  'use strict';
+  var server = require('gulp-livereload');
+
+  gulp.start('dev');
+  server.listen();
+
+  gulp.watch([SOURCE.jsxPath], ['js-dev']);
+
+});
 
 /* BUILD TASKS */
 gulp.task('build-html', function () {
@@ -32,4 +48,29 @@ gulp.task('build-html', function () {
     .pipe(gulp.dest(BUILD.htmlPath));
 });
 
-gulp.task('default');
+gulp.task('build-js', function () {
+  'use strict';
+  return gulp.src([
+      SOURCE.vendorPath,
+      SOURCE.jsPath
+    ])
+    .pipe(plugins.order([
+      'src/js/vendor/*',
+      'src/js/*'
+    ]))
+    .pipe(plugins.concat('App.js'))
+    .pipe(plugins.uglify({
+      wrap: true,
+      mangle: {
+        except: ['React', '$', '_', 'jQuery']
+      }
+    }))
+    .pipe(gulp.dest(BUILD.js));
+});
+
+gulp.task('build', function () {
+  gulp.start('build-js');
+  gulp.start('build-html');
+});
+
+gulp.task('default', ['watch-dev']);
